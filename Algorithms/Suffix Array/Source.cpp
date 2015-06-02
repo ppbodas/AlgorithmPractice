26fbbaa9da78f84b3851825a474cca44890fc6f1
Prathmesh Bodas <prathmesh@gmail.com>
2015-06-02 22:51:08 +0530

    Implementation

--- Algorithms/Suffix Array/Source.cpp
+++ Algorithms/Suffix Array/Source.cpp
@@ -5,9 +5,74 @@
 
 using namespace std;
 
-typedef vector<string> string_list;
+typedef vector<int> string_list;
+
+namespace SuffixArray
+{
+	int gap = 1;
+	vector<int> sa;
+	vector<int> pos;		//rank array
+	vector<int> tmp;		//rank array
+	
+	int len = 0;
+
+	bool suffixCmp(int i , int j)
+	{
+		if(pos[i] != pos[j])
+			return pos[i] < pos[j];
+
+		i += gap;
+		j += gap;
+
+		if(i < len && j < len)
+		{
+			return pos[i] < pos[j];
+		}
+		else
+			return i > j;
+	}
+
+	string_list createSuffixArray(string & str)
+	{
+		len = str.size();
+		sa.reserve(len);
+		pos.reserve(len);
+		tmp.reserve(len);
+
+		for(decltype(len) i = 0; i < len; ++i)
+		{
+			sa.push_back(i);
+			pos.push_back(str[i]);
+			tmp.push_back(0);
+		}
 
 
+		for(gap = 1;; gap = gap*2)
+		{
+			sort(sa.begin(), sa.end(), suffixCmp);
+			tmp[0] = 0;
+			for(decltype(len) i = 0; i < len-1; ++i)
+			{
+				tmp[i + 1] = tmp[i] + suffixCmp(sa[i], sa[i + 1]);
+			}
+
+			for(decltype(len) i = 0; i < len; ++i)
+			{
+				pos[sa[i]] = tmp[i]; 
+			}
+			if(tmp[len-1] == (len-1))
+				break;
+		}
+
+		/*string_list strList;
+		strList.reserve(len);
+		for(int i = 0;i < len; ++i)
+		{
+			strList.push_back(str.substr(sa[i]));
+		}*/
+		return sa;
+	}
+}
 string_list createSuffixArrayNaive(string & str)
 {
 	string_list strList;
@@ -16,9 +81,9 @@ string_list createSuffixArrayNaive(string & str)
 
 	for(decltype(len) i = 0; i < len; ++i)
 	{
-		strList.push_back(str.substr(i));
+		strList.push_back(i);
 	}
-	sort(strList.begin(), strList.end());
+	sort(strList.begin(), strList.end(), [str](int i, int j){return str.substr(i) < str.substr(j);});
 
 	return strList;
 }
@@ -28,6 +93,10 @@ int main()
 	cout << "Enter String: ";
 	std::getline(cin, str);
 	cout << "Input String is: " << str << endl;
-	auto strList = createSuffixArrayNaive(str);
-	copy(strList.begin(), strList.end(), ostream_iterator<string>(cout, "\n"));
+	//auto strList = createSuffixArrayNaive(str);
+
+	auto strList = SuffixArray::createSuffixArray(str);
+	copy(strList.begin(), strList.end(), ostream_iterator<int>(cout, "\n"));
+
+	cout << strList.size() <<endl;
 }
